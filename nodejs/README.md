91341f4962c61f258c4a4bccc2fb7900dedbefee
Lucas Machado <111795994+lucasmsdev@users.noreply.github.com>
2023-08-24 14:38:42 -0300

    Delete nodejs directory

--- nodejs/README.md
+++
@@ -1,5 +0,0 @@
-# Udemy-JavaScript-TypeScript-NodeJS
-Studies on javascript, typescript and nodejs using the udemy.com course
-
-
-
--- nodejs/aula52 - FOR/index.js
+++
@@ -1,5 +0,0 @@
-const frutas = ['maça', 'uva', 'banana', 'mexirica'];
-
-for (i = 0; i < frutas.length; i++) {
-    console.log(`Índice ${i}`, frutas[i])
-}
